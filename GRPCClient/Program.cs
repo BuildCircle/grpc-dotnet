1ffb5951e3a4ae0266b92e170b21eec19f327b23
JakeCummings <Jake.Cummings@rtgs.com>
2021-08-18 14:05:40 +0100

    first commit

---
+++ GRPCClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5000");
+            var client = new Game.GameClient(channel);
+            var reply = await client.PlayGameAsync(
+                              new GameRequest { Move = "ROCK" });
+            Console.WriteLine(reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GRPCServer/Services/GameService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GRPCServer
+{
+    public class GameService
+    {
+        private readonly ILogger<GameService> _logger;
+        public GameService(ILogger<GameService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public Task<EvaluateGame> PlayGame(GameRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new EvaluateGame
+            {
+                Message = "Your move: " + request.Move + ". Computer move: SCISSORS"
+            });
+                
+        }
+     }
+}
